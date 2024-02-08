f0e34a4df4a30b92004f8caad55aa0fb03208b2e
MendezCarlos-FS <freakinboyo@student.fullsail.edu>
2024-02-08 09:03:57 -0400

    c++ works in docker
    After building the c++ container and running it, it displays the required text.

---
+++ c/main.cpp
@@ -0,0 +1,14 @@
+#include <ctime>
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello World!" << endl;
+
+    time_t ttime = time(0);
+    char* dt = ctime(&ttime);
+
+    cout << dt;
+
+    return 0;
+}
