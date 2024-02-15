4c8c9c474fe5baee19f309b994db86b46213d35c
MendezCarlos-FS <freakinboyo@student.fullsail.edu>
2024-02-14 23:42:44 -0400

    Get by Id working
    The Get by Id method is working with error handling implemented.

--- node/controllers/contactsController.js
+++ node/controllers/contactsController.js
@@ -1,8 +1,8 @@
 const contactData = require("@jworkman-fs/asl/src/Data/contacts");
 const Pager = require("@jworkman-fs/asl/src/Util/Pager");
 const { PagerLimitExceededError } = require("../exceptions/pager");
-const { InvalidContactSchemaError } = require("@jworkman-fs/asl/src/Exception/index");
-const { sortContacts, filterContacts } = require("@jworkman-fs/asl/src/index");
+const { InvalidEnumError, InvalidOperatorError, InvalidContactResourceError } = require("@jworkman-fs/asl/src/Exception/index");
+const { sortContacts, filterContacts, ContactModel } = require("@jworkman-fs/asl/src/index");
 
 const validOperators = ["eq", "gt", "gte", "lt", "lte"];
 
@@ -17,10 +17,10 @@ function getContacts(req, res) {
             throw new PagerLimitExceededError("The limit per page cannot exceed 20.")
         }
         if (direction && direction !== "asc" && direction !== "desc") {
-            throw new InvalidContactSchemaError(`${direction} is not a valid direction. Valid directions are asc and desc.`);
+            throw new InvalidEnumError(`${direction} is not a valid direction. Valid directions are asc and desc.`);
         }
         if (filterOperator && !validOperators.includes(filterOperator)) {
-            throw new InvalidContactSchemaError(`${filterOperator} is not a valid operator. Valid operators are eq, gt, gte, lt or lte.`);
+            throw new InvalidOperatorError(`${filterOperator} is not a valid operator. Valid operators are eq, gt, gte, lt or lte.`);
         }
         const filteredData = filterContacts(filterBy, filterOperator, filterValue, contactData);
         const sortedData = sortContacts(filteredData, sort ? sort : "fname", direction ? direction : "asc");
@@ -30,23 +30,31 @@ function getContacts(req, res) {
         res.set("X-Page-Prev", pager.prev())
         res.status(200).json(pager.results());
     } catch(err) {
-        switch(err.name) {
-            case "PagerLimitExceededError":
-            case "PagerOutOfRangeError":
-            case "InvalidContactSchemaError":
-            case "NoContactsFoundError":
-                res.status(err.statusCode).json({
-                    message: err.message
-                });
-                break;
-            default:
-                const message = err.message ? err.message : "A bad request was received. There might be an invalid value in the request."
-                res.status(err.statusCode ? err.statusCode : 400).json(message);
-        }
+        errorHandling(err, res);
     }
+}
+
+function getContactById(req, res) {
+    const { id } = req.params;
+
+    try {
+        if (isNaN(Number(id))) {
+            throw new InvalidContactResourceError("The id provided is not a number.");
+        }
+
+        const contact = ContactModel.show(id);
+        res.status(200).json(contact);
+    } catch(err) {
+        errorHandling(err, res);
+    }
+}
+
+function errorHandling(err, res) {
+    const message = err.message ? err.message : "A bad request was received. There might be an invalid value in the request."
+    res.status(err.statusCode ? err.statusCode : 400).json({message});
 }
 
 module.exports = {
     getContacts,
-
+    getContactById,
 }
--- node/routes/contactsRouter.js
+++ node/routes/contactsRouter.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const { getContacts } = require("../controllers/contactsController");
+const { getContacts, getContactById } = require("../controllers/contactsController");
 const contactsRouter = express();
 
 contactsRouter.get("/contacts", getContacts);
-contactsRouter.get("/contacts/:id");
+contactsRouter.get("/contacts/:id", getContactById);
 contactsRouter.post("/contacts");
 contactsRouter.put("/contacts");
 contactsRouter.delete("/contacts");
