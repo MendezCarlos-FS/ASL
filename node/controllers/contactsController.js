62b33e84a4ecce66cc36351ab975a322895fcbe1
MendezCarlos-FS <freakinboyo@student.fullsail.edu>
2024-02-14 22:40:22 -0400

    Filtering and pagination complete
    Filtering is being implemented and error handled accordingly. Pagination is setting the response headers appropriately.

--- node/controllers/contactsController.js
+++ node/controllers/contactsController.js
@@ -2,10 +2,16 @@ const contactData = require("@jworkman-fs/asl/src/Data/contacts");
 const Pager = require("@jworkman-fs/asl/src/Util/Pager");
 const { PagerLimitExceededError } = require("../exceptions/pager");
 const { InvalidContactSchemaError } = require("@jworkman-fs/asl/src/Exception/index");
-const { sortContacts } = require("@jworkman-fs/asl/src/index");
+const { sortContacts, filterContacts } = require("@jworkman-fs/asl/src/index");
+
+const validOperators = ["eq", "gt", "gte", "lt", "lte"];
 
 function getContacts(req, res) {
     const {page, size, sort, direction} = req.query;
+    const filterBy = req.headers["x-filter-by"];
+    const filterOperator = req.headers["x-filter-operator"];
+    const filterValue = req.headers["x-filter-value"];
+
     try {
         if (size > 20) {
             throw new PagerLimitExceededError("The limit per page cannot exceed 20.")
@@ -13,20 +19,29 @@ function getContacts(req, res) {
         if (direction && direction !== "asc" && direction !== "desc") {
             throw new InvalidContactSchemaError(`${direction} is not a valid direction. Valid directions are asc and desc.`);
         }
-        const sortedData = sortContacts(contactData, sort ? sort : "fname", direction ? direction : "asc");
+        if (filterOperator && !validOperators.includes(filterOperator)) {
+            throw new InvalidContactSchemaError(`${filterOperator} is not a valid operator. Valid operators are eq, gt, gte, lt or lte.`);
+        }
+        const filteredData = filterContacts(filterBy, filterOperator, filterValue, contactData);
+        const sortedData = sortContacts(filteredData, sort ? sort : "fname", direction ? direction : "asc");
         const pager = new Pager(sortedData, page ? page : 1, size);
+        res.set("X-Page-Total", pager.pages)
+        res.set("X-Page-Next", pager.next())
+        res.set("X-Page-Prev", pager.prev())
         res.status(200).json(pager.results());
     } catch(err) {
         switch(err.name) {
             case "PagerLimitExceededError":
             case "PagerOutOfRangeError":
             case "InvalidContactSchemaError":
+            case "NoContactsFoundError":
                 res.status(err.statusCode).json({
                     message: err.message
                 });
                 break;
             default:
-                res.status(err.statusCode ? err.statusCode : 400).json(err);
+                const message = err.message ? err.message : "A bad request was received. There might be an invalid value in the request."
+                res.status(err.statusCode ? err.statusCode : 400).json(message);
         }
     }
 }
